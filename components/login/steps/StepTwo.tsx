26b317fa643c1673dc10ecb459f375f0703fac2b
francis <fbalimuttajjo@gmail.com>
2023-01-21 16:33:11 +0300

    chore(ui): fix ts error

--- components/login/steps/StepTwo.tsx
+++ components/login/steps/StepTwo.tsx
@@ -1,10 +1,10 @@
-import * as Yup from "yup";
-import React from "react";
-import { Formik, Field } from "formik";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import Input from "../../reusableComponents/Input";
-import { TextInput } from "react-native-paper";
-import { Props } from "../useRegisterFns";
+import * as Yup from 'yup';
+import React from 'react';
+import {Formik, Field} from 'formik';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import Input from '../../reusableComponents/Input';
+import {TextInput} from 'react-native-paper';
+import {Props} from '../useRegisterFns';
 
 type FormProps = {
   handleBack: () => void;
@@ -16,22 +16,21 @@ export const Form = (props: FormProps) => {
   const validationSchema = Yup.object().shape({
     Email: Yup.string()
       .trim()
-      .email("invalid Email")
-      .required("Email is required")
-      .label("Email"),
+      .email('invalid Email')
+      .required('Email is required')
+      .label('Email'),
   });
 
-  const handleSubmit = (value: { Email: string }) => {
+  const handleSubmit = (value: {Email: string}) => {
     props.setFormValues(value.Email);
   };
 
   return (
     <Formik
-      initialValues={{ Email: props.formValues.email }}
+      initialValues={{Email: props.formValues.email}}
       validationSchema={validationSchema}
-      onSubmit={handleSubmit}
-    >
-      {({ handleSubmit }) => (
+      onSubmit={handleSubmit}>
+      {({handleSubmit}) => (
         <View>
           <View style={styles.container}>
             <Text> Hi {props.formValues.firstName}, Enter your Email </Text>
@@ -41,21 +40,18 @@ export const Form = (props: FormProps) => {
               label="Email"
               left={<TextInput.Icon name="email" color="#bbbdbd" />}
             />
-
             <TouchableOpacity
               activeOpacity={0.6}
               style={styles.btn}
-              onPress={props.handleBack}
-            >
-              <Text style={styles.login_text}>back</Text>
+              onPress={handleSubmit}>
+              <Text style={styles.login_text}>Next</Text>
             </TouchableOpacity>
 
             <TouchableOpacity
               activeOpacity={0.6}
               style={styles.btn}
-              onPress={handleSubmit}
-            >
-              <Text style={styles.login_text}>Next</Text>
+              onPress={props.handleBack}>
+              <Text style={styles.login_text}>back</Text>
             </TouchableOpacity>
           </View>
         </View>
@@ -66,16 +62,16 @@ export const Form = (props: FormProps) => {
 export default Form;
 
 const styles = StyleSheet.create({
-  container: { width: "95%", alignSelf: "center", paddingVertical: "15%" },
-  login_text: { color: "#fff", fontWeight: "bold", fontSize: 18 },
+  container: {width: '95%', alignSelf: 'center', paddingVertical: '15%'},
+  login_text: {color: '#fff', fontWeight: 'bold', fontSize: 18},
 
   btn: {
-    width: "100%",
-    alignItems: "center",
-    justifyContent: "center",
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
     paddingVertical: 15,
     borderRadius: 7,
     marginTop: 20,
-    backgroundColor: "#326fa8",
+    backgroundColor: '#326fa8',
   },
 });
